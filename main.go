acd30709cc353bfc4567d34859e418ae29378312
Yonatan Koren <me@yonatankoren.com>
2022-04-25 17:59:03 +0300

    Feat: Add Container Build and Unit Tests (#7)
    * Add Dockerfile support and container build pipeline.
    
    * Make pipeline step names appropriate in goreleaser pipeline.
    
    * Add unit tests.
    
    * Add CI pipeline.
    
    Co-authored-by: Yonatan Koren <10080107+korenyoni@users.noreply.github.com>

--- main.go
+++ main.go
@@ -13,35 +13,48 @@ var envKeys = map[string]string{
 	"port": "PORT",
 }
 
-func main() {
-	port, err := getEnv(envKeys["port"])
-	if err != nil {
-		log.Fatal(err)
-	}
-	r := gin.Default()
-	r.StaticFile("/favicon.ico", "./site/favicon.ico")
-	r.StaticFile("/style.css", "./site/style.css")
-	r.StaticFile("/logo.png", "./site/logo.png")
-	r.LoadHTMLGlob("site/templates/*")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"sourceCodeURI": "https://github.com/korenyoni/codefresh-web-app-test",
-			"codefreshURI":  "https://g.codefresh.io/",
-			"logo":          "/logo.png",
-		})
-	})
-	r.NoRoute(func(c *gin.Context) {
-		c.HTML(http.StatusOK, "404.tmpl", gin.H{
-			"logo": "/logo.png",
-		})
-	})
-	r.Run(fmt.Sprintf(":%s", port))
-}
-
 func getEnv(key string) (string, error) {
 	val, ok := os.LookupEnv(key)
 	if !ok {
 		return "", errors.New(fmt.Sprintf("%s not set", key))
 	}
 	return val, nil
+}
+
+func BuildRouter() *gin.Engine {
+	router := gin.Default()
+	router.StaticFile("/favicon.ico", "./site/favicon.ico")
+	router.StaticFile("/style.css", "./site/style.css")
+	router.StaticFile("/logo.png", "./site/logo.png")
+	router.LoadHTMLGlob("site/templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"sourceCodeURI": "https://github.com/korenyoni/codefresh-web-app-test",
+			"codefreshURI":  "https://g.codefresh.io/",
+			"logo":          "/logo.png",
+		})
+	})
+	router.GET("/health-check", func(c *gin.Context) {
+		c.String(http.StatusOK, "alive")
+	})
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.tmpl", gin.H{
+			"logo": "/logo.png",
+		})
+	})
+	return router
+}
+
+func main() {
+	port, err := getEnv(envKeys["port"])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := BuildRouter()
+	err = router.Run(fmt.Sprintf(":%s", port))
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var addr string = ":8080"
+var path404 string = "/askdasjldka"
+
+func TestIndexHandler(t *testing.T) {
+	router := BuildRouter()
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Fail()
+	}
+}
+
+func TestHealthCheckHandler(t *testing.T) {
+	router := BuildRouter()
+
+	req, err := http.NewRequest("GET", "/health-check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Fail()
+	}
+}
+
+func TestDefaultHandler(t *testing.T) {
+	router := BuildRouter()
+
+	req, err := http.NewRequest("GET", path404, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusNotFound {
+		t.Fail()
+	}
+}
